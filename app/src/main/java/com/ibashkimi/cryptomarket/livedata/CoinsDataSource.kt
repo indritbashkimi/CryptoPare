1e0fc990ee65a3d4971dc1f34b20c623375e2951
Indrit Bashkimi <indrit.bashkimi@gmail.com>
2018-03-11 14:23:23 +0100

    Use PageKeyedDataSource.

--- app/src/main/java/com/ibashkimi/cryptomarket/livedata/CoinsDataSource.kt
+++ app/src/main/java/com/ibashkimi/cryptomarket/livedata/CoinsDataSource.kt
@@ -1,39 +1,35 @@
 package com.ibashkimi.cryptomarket.livedata
 
-import android.arch.paging.ItemKeyedDataSource
+import android.arch.paging.PageKeyedDataSource
 import com.ibashkimi.cryptomarket.data.DataManager
 import com.ibashkimi.cryptomarket.model.Coin
 import com.ibashkimi.cryptomarket.settings.PreferenceHelper
 
 
-class CoinsDataSource : ItemKeyedDataSource<Int, Coin>() {
-
-    override fun loadBefore(params: LoadParams<Int>, callback: LoadCallback<Coin>) {
+class CoinsDataSource : PageKeyedDataSource<Int, Coin>() {
+    override fun loadBefore(params: LoadParams<Int>, callback: LoadCallback<Int, Coin>) {
         // Ignored
     }
 
-    override fun loadInitial(params: LoadInitialParams<Int>, callback: LoadInitialCallback<Coin>) {
+    override fun loadInitial(params: LoadInitialParams<Int>, callback: LoadInitialCallback<Int, Coin>) {
         DataManager.getCoins(0, params.requestedLoadSize,
                 PreferenceHelper.currency,
                 onSuccess = {
-                    callback.onResult(it)
+                    callback.onResult(it, null, params.requestedLoadSize)
                 },
                 onFailure = {
                 })
     }
 
-    override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Coin>) {
+    override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Int, Coin>) {
         DataManager.getCoins(params.key, params.requestedLoadSize,
                 PreferenceHelper.currency,
                 onSuccess = {
-                    callback.onResult(it)
+                    callback.onResult(it,
+                            if (it.size == params.requestedLoadSize) it.last().rank.toInt() else null)
                 },
                 onFailure = {
 
                 })
-    }
-
-    override fun getKey(item: Coin): Int {
-        return item.rank.toInt()
     }
 }
