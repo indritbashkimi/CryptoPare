0f6ca7da22940ef8d8de1a26dabb8d72f724eaa6
Indrit Bashkimi <indrit.bashkimi@gmail.com>
2021-03-13 19:29:18 +0100

    Fix nav controller not found

--- app/src/main/java/com/ibashkimi/cryptomarket/HomeFragment.kt
+++ app/src/main/java/com/ibashkimi/cryptomarket/HomeFragment.kt
@@ -5,7 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -14,13 +14,13 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val navController =
-            Navigation.findNavController(root.findViewById(R.id.home_nav_host_fragment))
-        root.findViewById<BottomNavigationView>(R.id.bottom_navigation).apply {
-            setupWithNavController(navController)
-        }
-        return root
+    ): View? = inflater.inflate(R.layout.fragment_home, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        (childFragmentManager.findFragmentById(R.id.home_nav_host_fragment) as NavHostFragment)
+            .navController.let { navController ->
+                view.findViewById<BottomNavigationView>(R.id.bottom_navigation)
+                    .setupWithNavController(navController)
+            }
     }
 }
